f04f1641e241eff4fbd8f8c41f37af570647ffd9
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-06-17 19:56:58 +0200

    Added react toastify

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import Layout from '../components/Layout';
 import '../styles/globals.css' ;
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
-   <Component {...pageProps} />
+    <Component {...pageProps} />
+    <ToastContainer />
   </Layout>
   )
 }
--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -1,10 +1,14 @@
-import Container from "@mui/material/Container";
+import {useEffect} from "react";
 import Grid from "@mui/material/Grid";
 import JobCard from "../../components/jobs/JobCard";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import {toast} from "react-toastify";
 
 const JobsList = () => {
+  useEffect(() => {
+    toast.info("Fetching jobs list");
+  }, []);
   const jobs = Array(20).fill(0);
   return (
     <Grid container justifyContent={'center'} py={12} spacing={2}>
